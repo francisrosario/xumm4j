9c3ebf71f8230a318925e2333ce5a3b7934b95e4
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-08-01 03:47:37 +0800

    DeserializeITTest.java: Add JUnit test for DeserializeIT

---
+++ src/test/java/com/fl/xumm4j/sdk/DeserializeITTest.java
@@ -0,0 +1,52 @@
+package com.fl.xumm4j.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeserializeITTest {
+
+    @Test
+    void ping() {
+    }
+
+    @Test
+    void curatedAssets() {
+    }
+
+    @Test
+    void details() {
+    }
+
+    @Test
+    void currencies() {
+    }
+
+    @Test
+    void storage() {
+    }
+
+    @Test
+    void kycPublic() {
+    }
+
+    @Test
+    void kycState() {
+    }
+
+    @Test
+    void getPayload() {
+    }
+
+    @Test
+    void payload() {
+    }
+
+    @Test
+    void rates() {
+    }
+
+    @Test
+    void deletePayload() {
+    }
+}
