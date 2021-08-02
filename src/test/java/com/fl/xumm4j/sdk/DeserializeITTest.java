ff1cfed80c9bde28bb99471f87123222d537af7f
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-08-03 00:11:48 +0800

    DeserializeITTest.java: Add comment why it's empty

--- src/test/java/com/fl/xumm4j/sdk/DeserializeITTest.java
+++ src/test/java/com/fl/xumm4j/sdk/DeserializeITTest.java
@@ -8,45 +8,56 @@ class DeserializeITTest {
 
     @Test
     void ping() {
+        // WIP
     }
 
     @Test
     void curatedAssets() {
+        // WIP
     }
 
     @Test
     void details() {
+        // WIP
     }
 
     @Test
     void currencies() {
+        // WIP
     }
 
     @Test
     void storage() {
+        // WIP
     }
 
     @Test
     void kycPublic() {
+        // WIP
     }
 
     @Test
     void kycState() {
+        // WIP
     }
 
     @Test
     void getPayload() {
+        // WIP
     }
 
     @Test
     void payload() {
+        // WIP
     }
 
     @Test
     void rates() {
+        // WIP
     }
 
     @Test
     void deletePayload() {
+        // WIP
     }
 }
