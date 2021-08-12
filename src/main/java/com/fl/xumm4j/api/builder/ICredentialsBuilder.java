7e1e41e37ca54755695ff31e96acd348e59b65d6
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-08-12 10:20:31 +0800

    Add javadocs to ICredentialsBuilder.java

--- src/main/java/com/fl/xumm4j/api/builder/ICredentialsBuilder.java
+++ src/main/java/com/fl/xumm4j/api/builder/ICredentialsBuilder.java
@@ -2,24 +2,39 @@ package com.fl.xumm4j.api.builder;
 
 import com.fl.xumm4j.sdk.builder.CredentialsBuilder;
 
+/**
+ * The interface Credentials builder.
+ */
 public interface ICredentialsBuilder {
+    /**
+     * The constant ERROR_APIKEY.
+     */
     String ERROR_APIKEY = "Empty API Key";
+    /**
+     * The constant ERROR_SECRET_KEY.
+     */
     String ERROR_SECRET_KEY = "Empty Secret Key";
 
     /**
-     * @param apiKey
-     * @return
+     * Api key credentials builder . builder.
+     *
+     * @param apiKey the API Key obtained from <a href="https://apps.xumm.dev/">https://apps.xumm.dev/</a>
+     * @return the credentials builder . builder
      */
     CredentialsBuilder.builder apiKey(String apiKey);
 
     /**
-     * @param secretKey
-     * @return
+     * Secret key credentials builder . builder.
+     *
+     * @param secretKey the API Secret obtained from <a href="https://apps.xumm.dev/">https://apps.xumm.dev/</a>
+     * @return the credentials builder . builder
      */
     CredentialsBuilder.builder secretKey(String secretKey);
 
     /**
-     * @return
+     * Build credentials builder.
+     *
+     * @return the credentials builder
      */
     CredentialsBuilder build();
 }
